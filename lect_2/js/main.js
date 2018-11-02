95e20f6c3e758975932f59a3f9a370246c6a08a5
Igor <basprogrod@bk.ru>
2018-11-02 16:50:14 +0300

    дз_1 испарвлено

--- lect_2/js/main.js
+++ lect_2/js/main.js
@@ -1,18 +1,19 @@
 function compare (a, b) {
 
  if (a > b) {
- 	return false
+ 	return true
  } else {
- 	return true	
+ 	return false	
  }
 
 }
 console.log(compare(1, 2));
 console.log(compare(9, 2));
+
 function concat (str) {
 	return 'Вы ввели: ' + str
 }
-console.log('//////////////////////////////////////////////////////////////')
+
 
 
 
@@ -21,9 +22,8 @@ console.log('//////////////////////////////////////////////////////////////')
 
 
 function chek (val) {
-	if (val === null || val === undefined) return "Значение: " + val;
- return "Значение не является " + null + " или " + undefined;
-
+ if (val === null || val === undefined) return true;
+ return false;
 }
 
 
@@ -34,7 +34,7 @@ console.log(chek("null"));
 console.log(chek(null));
 console.log(chek());
 
-console.log('//////////////////////////////////////////////////////////////')
+
 
 
 
@@ -54,7 +54,7 @@ function addParameter (obj) {
 addParameter(a);
 console.log(a);
 
-console.log('//////////////////////////////////////////////////////////////')
+
 
 
 
@@ -65,22 +65,36 @@ console.log('//////////////////////////////////////////////////////////////')
 
 
 function getInterval (n) {
- // console.log(typeof n + ' ' + n)
+
 	if (n === 0 || (typeof n) === 'string' || (typeof n) === 'object' || null || undefined) {
 		console.log('введите число отличное от 0')
-		return;
-	}
+		return false;
 
- if (n > 0) {
- 	for (var i = 0; i < n; i++) console.log(i);
+	}
+ var start = 0;
+ var end = n;
+
+ if (n < 0) {
+ 	end = Math.sqrt(n*n);
+
+ } if (n === 0){
+    return false;
+
  } else {
- 		for (var i = 0; i > n; i--) console.log(i);
+ 	start -= end;
+
  }
+
+
+ // console.log(mod + '!!!');
+ for (var i = ++start; i < end; i++) console.log(i);
+ // console.log(-mod)
 }
 getInterval('ffff');
 getInterval(0);
 getInterval({});
 getInterval();
 getInterval(null);
-getInterval(15);
-getInterval(-15);
+// getInterval(15);
+
+getInterval(-7);
